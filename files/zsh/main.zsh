eb1bec043df1613abe8d223d2c73a6ad661596a3
cerico <garethrobertlee@gmail.com>
2023-10-10 17:13:00 +0200

    fix: search for functions more accurately
    grep for any number of spaces followed by () followed by any number of spaces followed by {

--- files/zsh/main.zsh
+++ files/zsh/main.zsh
@@ -62,7 +62,7 @@ m () { # Execute nearest Makefile up directory tree
 
 viz () {
   [[ ! $1 ]] && vi ~/.zsh/_trialling.zsh && return
-  local result=$(grep -n "^$1.*{" ~/.zsh/*.zsh)
+  local result=$(grep -n "^$1[[:space:]]*()[[:space:]]*{" ~/.zsh/*.zsh)
   local func=$(echo "$result" | awk -F':' '{print $1}')
   local line=$(echo "$result" | awk -F':' '{print $2}')
   [[ ! $func ]] && vi ~/.zsh/_trialling.zsh && return
