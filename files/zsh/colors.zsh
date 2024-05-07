6838c226795bf79dae23ea36b0f956ee1559d9cf
cerico <garethrobertlee@gmail.com>
2024-05-07 04:19:33 +0100

    feat: add peacock function
    change peacock values on fly

--- files/zsh/colors.zsh
+++ files/zsh/colors.zsh
@@ -13,3 +13,38 @@ ColorBlue(){
 ColorCyan(){
   echo -ne $cyan$1$clear
 }
+
+peacock () {
+  [ ! $1 ] && return
+  if [ ! -f .vscode/settings.json ]
+    then
+    mkdir -p .vscode
+    cp ~/Library/Application\ Support/Code/User/settings.json .vscode/settings.json
+  fi
+  declare -A colors=(
+    [yellow]='#f6ef32'
+    [white]='#faf0d4'
+    [green]='#42b883'
+    [pink]='#FF757D'
+    [red]='#e3664a'
+  )
+  local color="${colors[$1]}"
+  if [ ! $color ];
+    then
+    echo "available colors"
+    for key in "${(k)colors[@]}"; do
+      echo "$key"
+    done
+    return
+  fi
+  echo "changing peacock color to $color"
+  local replace=$(grep  "peacock.color" .vscode/settings.json | awk -F"\"" '{print $4}')
+  if [ $replace ]
+    then
+    sed -i "" -e s/$replace/$color/g .vscode/settings.json
+  else
+     sed -i "" -e '2i\ 
+  "peacock.color": "#gggggg",' .vscode/settings.json
+    sed -i "" -e s/#gggggg/$color/g .vscode/settings.json
+  fi
+}
