1656f5fb425c4eccaf3b6a9a6aae36838b49f092
cerico <garethrobertlee@gmail.com>
2024-05-20 05:38:39 +0100

    feat: update peacock function to backup settings

--- files/zsh/colors.zsh
+++ files/zsh/colors.zsh
@@ -15,19 +15,14 @@ ColorCyan(){
 }
 
 peacock () {
-  [ ! $1 ] && return
-  if [ ! -f .vscode/settings.json ]
-    then
-    mkdir -p .vscode
-    cp ~/Library/Application\ Support/Code/User/settings.json .vscode/settings.json
-  fi
-  declare -A colors=(
+    declare -A colors=(
     [yellow]='#f6ef32'
     [white]='#faf0d4'
     [green]='#42b883'
     [pink]='#FF757D'
     [red]='#e3664a'
   )
+
   local color="${colors[$1]}"
   if [ ! $color ];
     then
@@ -37,14 +32,29 @@ peacock () {
     done
     return
   fi
+
+  if [ ! $1 ]
+    then
+    echo "please provide a color"
+    return
+  fi
+  if [ ! -f .vscode/settings.json ]
+    then
+    mkdir -p .vscode
+    cp ~/Library/Application\ Support/Code/User/settings.json .vscode/settings.json
+  else
+    cp .vscode/settings.json .vscode/origsettings.json
+  fi
+
   echo "changing peacock color to $color"
   local replace=$(grep  "peacock.color" .vscode/settings.json | awk -F"\"" '{print $4}')
   if [ $replace ]
     then
     sed -i "" -e s/$replace/$color/g .vscode/settings.json
   else
-     sed -i "" -e '2i\ 
+     sed -i "" -e '2i\
   "peacock.color": "#gggggg",' .vscode/settings.json
     sed -i "" -e s/#gggggg/$color/g .vscode/settings.json
   fi
+  code .
 }
