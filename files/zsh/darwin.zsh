7de09ac946bb6f636213d286d11ff8dd26755419
cerico <garethrobertlee@gmail.com>
2025-08-02 11:53:19 +0200

    feat: add autotz cron, remove batt and dfh crons

--- files/zsh/darwin.zsh
+++ files/zsh/darwin.zsh
@@ -5,30 +5,19 @@ brewser=$(stat -f "%Su" $(which brew))
 alias brew='sudo -Hu '$brewser' brew'
 
 batt () {
-  [[ $1 = "clear" ]] && echo -n > ~/.batt
   if (pmset -g batt | grep Internal | grep estimate > /dev/null); then
     batt=$(pmset -g batt | grep Internal |awk -F' ' '{print $3,$4}' | tr -d \;)
   else
     batt=$(pmset -g batt | grep Internal |awk -F' ' '{print $3,$4,$5,$6}' | tr -d \;)
   fi
-  last=`tail -n 1 ~/.batt | awk -F " " '{ print $3}'`
-  this=`echo $batt | awk -F " " '{ print $2}'`
   date=`date "+%H:%M"`
-  cat ~/.batt
   echo $date $batt
-  [[ $last = 'charged' ]] && [[ $this = 'charged' ]] && return
-  [[ $1 = "cron" ]] && echo $date $batt >> ~/.batt
 }
 
 dfh () {
-  [[ $1 = "clear" ]] && echo -n > ~/.dfh
   dfh=`df -h | head -2 | tail -1 | awk -F " " '{print $4}'`
   date=`date "+%H:%M"`
-  last=`tail -n 1 ~/.dfh | awk -F " " '{ print $2}'`
-  [[ -f ~/.dfh ]] && cat ~/.dfh
   echo $date $dfh
-  [[ $last = $dfh ]] && return
-  [[ $1 = "cron" ]] && echo $date $dfh >> ~/.dfh
 }
 
 nfy () {
